8a22c273f8b08fe13526dad90f6c106a17093d61
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-10-19 06:41:57 +0200

    middle of 127

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,8 +1,9 @@
-import { useFetch } from '../hook/useFetch';
+import { useCounter, useFetch } from '../hook';
 
 export const MultipleCustomHooks = () => {
+  const { counter, increment } = useCounter(1);
   const { data, isLoading, hasError } = useFetch(
-    'https://www.breakingbadapi.com/api/quotes/1',
+    `https://www.breakingbadapi.com/api/quotes/${counter}`,
   );
   const { author, quote } = !!data && data[0];
   // if (isLoading) {
@@ -12,16 +13,25 @@ export const MultipleCustomHooks = () => {
     <>
       <h1>BrakingBad Quotes</h1>
       <hr />
-      {isLoading ? (
+      {/* {isLoading ? (
         <div className="alert alert-info text-center">Loading...</div>
       ) : (
         <blockquote className="blockquote text-end">
           <p className="mb-1">{quote}</p>
           <footer className="blockquote-footer">{author}</footer>
         </blockquote>
-      )}
+      )} */}
 
-      <button className="btn btn-primary">Next Quote</button>
+      <loadingQuote />
+      <Quote />
+
+      <button
+        className="btn btn-primary"
+        disable={isLoading}
+        onClick={() => increment()}
+      >
+        Next Quote
+      </button>
     </>
   );
 };
