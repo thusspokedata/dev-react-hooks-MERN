7de4c9c4d5a4274f877db8cb8d29e228b1d89ae0
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-11-06 02:16:45 +0100

    up to chapter 190

--- 07-heroes-spa/src/router/AppRouter.jsx
+++ 07-heroes-spa/src/router/AppRouter.jsx
@@ -1,15 +1,13 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
-import { DcPage, MarvelPage } from '../heroes/pages';
+import { HeroesRoutes } from '../heroes';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
